dfe8ebeb40964180ddee3162f7b145058e951dfc
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-04-14 09:21:46 -0300

    Update var-let-const.js e Readme

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
  Este repositório faz parte do portfólio de aprendizado do curso de pós graduação em desenvolvimento fullstack da PUC-RS, da disciplina de Programação Orientada a Objetos.
 
  <p>
- <a href="var-let-const.js">var-let-const.js</a>
+ <a href="var-let-const.js">var-let-const.js: </a> Estudo sobre visibilidade. 
+ <ul>
+ <li>var: escopo de função,</li>
+ <li>let: escopo de bloco (variável local),</li>
+ <li>const: escopo de bloco.</li>
+</ul>
--- var-let-const.js
+++ var-let-const.js
@@ -1,113 +1,118 @@
 //var 01
 console.log("****** VAR 01 ******");
 function testVar01() {
-    var var01 = "var 01";
+    var var01 = "var 01 criada antes do bloco";
     {
-        var var02 = "var 02";
-        console.log("01a: " + var01);
-        console.log("01b: " + var02);
-        console.log();
+        var var02 = "var 02 criada dentro do bloco";
+        console.log("01a chamada dentro do bloco: " + var01);
+        console.log("01b chamada dentro do bloco: " + var02);        
     }
-    console.log("02a: " + var01);
-    console.log("02b: " + var02);
+    console.log("02a chamada depois do bloco: " + var01);
+    console.log("02b chamada depois do bloco: " + var02);
 };
 
 testVar01();
-console.log("****** VAR 02 ******");
+console.log();
 
 //var 02
+console.log("****** VAR 02 ******");
+
 function testVar02() {
-    var var01 = "var 01";
+    var var01 = "var 01 criada antes do if";
     if (1 == 0) {
-        var var02 = "var 02";
-        console.log("01a: " + var01);
-        console.log("01b: " + var02);
-        console.log();
+        var var02 = "var 02 criada dentro do if";
+        console.log("01a chamada dentro do if: " + var01);
+        console.log("01b chamada dentro do if: " + var02);        
     }
     
-    console.log("02a: " + var01);
-    console.log("02b: " + var02);//undefined
-    console.log();
+    console.log("02a chamada depois do if: " + var01);
+    console.log("02b chamada depois do if: " + var02);//undefined
 };
 
 testVar02();
-console.log("****** VAR 03 ******");
+console.log();
 
 //var 03
+console.log("****** VAR 03 ******");
+
 function testVar03() {
-    var var01 = "var 01";
+    var var01 = "var 01 criada antes do bloco";
     {
-        console.log("01a: " + var01);
-        var var01 = "var 02";
-        console.log("02a: " + var01);
-        console.log();
+        console.log("01a chamada dentro do bloco: " + var01);
+        var var01 = "var 02 criada dentro do bloco";
+        console.log("02a chamada dentro do bloco: " + var01);
     }
-    var var01 = "var 03";
-    console.log("03a: " + var01);    
-    console.log();
+    var var01 = "var 03 criada depois do bloco";
+    console.log("03a chamada depois do bloco: " + var01);        
 };
 
 testVar03();
+console.log();
 
 //let 01
 console.log("****** LET 01 ******");
+
 function testLet1(){
-    let let01 = "let 01";
+    let let01 = "let 01 criada antes do bloco";
     {
-        let let02 = "let 02";
-        console.log("01a: " + let01);
-        console.log("01b: " + let02);
+        let let02 = "let 02 criada dentro do bloco";
+        console.log("01a chamada dentro do bloco: " + let01);
+        console.log("01b chamada dentro do bloco: " + let02);
     }
-    console.log("02a: " + let01);    
+    console.log("02a chamada depois do bloco: " + let01);    
 }
 testLet1();
 
 //let 02
 console.log("****** LET 02 ******");
+
 function testLet2(){
-    let let01 = "let 01";
+    let let01 = "let 01 criada antes do bloco";
     {
-        let let02 = "let 02";
-        console.log("01a: " + let01);
-        console.log("01b: " + let02);
+        let let02 = "let 02 criada dentro do bloco";
+        console.log("01a chamada dentro do bloco: " + let01);
+        console.log("01b chamada dentro do bloco: " + let02);
     }
-    console.log("02a: " + let01);
-    // console.log("02b: " + let02); ReferenceError: let02 is not defined
+    console.log("02a chamada depois do bloco: " + let01);
+    // console.log("02b chamada depois do bloco: " + let02); ReferenceError: let02 is not defined
 }
 testLet2();
 
 //let 03
 console.log("****** LET 03 ******");
+
 function testLet3(){
-    let let01 = "let 01";
+    let let01 = "let 01 criada antes do bloco";
     {
-        // console.log("01a: " + let01); ReferenceError: Cannot access 'let01' before initialization
-        let let01 = "let 02";
-        console.log("02a: " + let01);
+        // console.log("01a chamada dentro do bloco: " + let01); ReferenceError: Cannot access 'let01' before initialization
+        let let01 = "let 02 criada dentro do bloco";
+        console.log("02a chamada dentro do bloco: " + let01);
     }
-    console.log("03a: " + let01);
+    console.log("03a chamada depois do bloco: " + let01);
     
 }
 testLet3();
 
 //const 01
 console.log("****** CONST 01 ******");
+
 function testConst1(){
-    const const01 = "const 01";
+    const const01 = "const 01 criada antes do bloco";
     {
-        const const02 = "const 02";
-        console.log("01a: " + const01);
-        console.log("01b: " + const02);
+        const const02 = "const 02 criada dentro do bloco";
+        console.log("01a chamada dentro do bloco: " + const01);
+        console.log("01b chamada dentro do bloco: " + const02);
     }
-    // const01 = "novo valor"; TypeError: Assignment to constant variable.
-    console.log("02a: " + const01);
-    // console.log("02b: " + const02); ReferenceError: const02 is not defined       
+    // const01 = "novo valor criada antes do bloco, alterada depois do bloco"; TypeError: Assignment to constant variable.
+    console.log("02a chamada depois do bloco: " + const01);
+    // console.log("02b criada dentro do bloco: " + const02); ReferenceError: const02 is not defined       
 }
 
 testConst1();
 
 //const 02
 console.log("****** CONST 02 ******");
+
 function testConst2(){
     const const01 = {propriedade: "valor"};
     console.log("01a: " + const01.propriedade);
@@ -125,7 +130,7 @@ function testConst3(){
     console.log("01a: " + const01.propriedade);
     
     // const01 = {propriedade: "novo valor"}; TypeError: Assignment to constant variable.
-    console.log("02a: " + const01.propriedade);    //02a: [object Object]
+    console.log("02a: " + const01.propriedade);
 }
 
 testConst3();
